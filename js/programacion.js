2d8ee58fd76f2a52681e007e77c9f0f59607d5a6
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2017-06-02 13:55:40 -0500

    arreglar errores

--- js/programacion.js
+++ js/programacion.js
@@ -1,27 +1,24 @@
-// Click function for show the Modal
-
+// al hacer click muestra el Modal
 $(".show").on("click", function () {
     $(".mask").addClass("active");
 });
 
-// Function for close the Modal
-
+// al hacer click se cierra el Modal
 function closeModal() {
     $(".mask").removeClass("active");
 }
 
-// Call the closeModal function on the clicks/keyboard
-
+// Cerrar el Modal con el teclado con la tecla ESC
 $(".close, .mask").on("click", function () {
     closeModal();
 });
-
 $(document).keyup(function (e) {
     if (e.keyCode == 27) {
         closeModal();
     }
 });
 
+//Ir a la URL de UM Virtual
 $(".url").on('click', function(){
      window.location = "https://umvirtual.org";    
 });
