64aa3d52833c27604961aab9881de9a63415c1d8
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2017-06-13 18:47:50 -0500

    agregar video en la parte superior

--- js/programacion.js
+++ js/programacion.js
@@ -19,6 +19,33 @@ $(document).keyup(function (e) {
 });
 
 //Ir a la URL de UM Virtual
-$(".url").on('click', function(){
-     window.location = "https://umvirtual.org";    
+$(".url").on('click', function () {
+    window.location = "https://umvirtual.org";
+});
+
+
+// al hacer click muestra el Modal
+$(".mostrar").on("click", function () {
+    $(".mask").addClass("activo");
+    $('iframe').attr({
+        'src': 'https://www.youtube-nocookie.com/embed/j6UIlgu3UYg?rel=0&amp;showinfo=0?ecver=1?&autoplay=1&yt:stretch=16:9'
+    });
+});
+
+// al hacer click se cierra el Modal
+function closeModal2() {
+    $(".mask").removeClass("activo");
+    $('iframe').attr({
+        'src': ''
+    });
+}
+
+// Cerrar el Modal con el teclado con la tecla ESC
+$(".close, .mask").on("click", function () {
+    closeModal2();
+});
+$(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+        closeModal2();
+    }
 });
